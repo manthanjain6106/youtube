68286dbfe95b6c26f5128267498a365c77ca5e4f
manthanjain6106 <jainmanthan6106@gmail.com>
2025-04-22 15:06:34 +0530

    update cloudinary and add multer in middleeares

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -11,15 +11,29 @@ import fs from "fs"
     });
 
 
-    // Upload an image
-    const uploadResult = await cloudinary.uploader
-    .upload(
-        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-            public_id: 'shoes',
+    const uploadOnCloudinary = async (locaFilePath) =>{
+        try {
+            if (!locaFilePath) return null
+
+            // Upload an image
+
+            const response = await cloudinary.uploader.upload(locaFilePath, {
+                resource_type: "auto"
+            })
+
+            // file hase been uploaded successfully
+
+            console.log("file is uploaded on cloudinary", response.url);
+            
+            return response;
+
+
+                
+        } catch (error) {
+            fs.unlinkSync(locaFilePath) //remove locally saved temporary file as the uoload operation got failed
+
+            return null;
         }
-    )
-    .catch((error) => {
-        console.log(error);
-    });
- 
- console.log(uploadResult);
+    }
+
+    export { uploadOnCloudinary };
